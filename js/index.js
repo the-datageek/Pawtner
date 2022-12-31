dff6a3cf657059f67d5724e37dd9289460b5bca5
Faith Moraa <moraaf770@gmail.com>
2022-12-31 11:01:16 +0300

    Changes ti event listener for login page

--- js/index.js
+++ js/index.js
@@ -1,40 +1,11 @@
-/*--//URL
+ //EventListener for Login Form
+ $('.signup').hide();
 
-//fetch data
- let http = new XMLHttpRequest();
-
- //prepare request with open() method
- http.open("get", "pets.json", true);
-
- //send the request
- http.send();
-
- //catch the response
- //check the onload eventListener
-
- http.onload = function(){
-	//check for ready state and status properties
-	if(this.readyState == 4 && this.status == 200){
-
-		let pets = JSON.parse(this.responseText);
-		let output ="";
-
-		for(let item of pets){
-			output+= `
-			<div class = "pet">
-				<image src="${item.image}" alt="${item.image}">
-				<h4 class="name">${item.name}</h4>
-				<h8 class="gender">${item.gender}</h8>
-				<h5 class="description">${item.description}</h5>
-				<p class ="wishlist">
-				<i class="uil uil-heart"></i></p>
-				<button4>Adopt</button4>
-			</div>
-			`;
-		}
-		document.querySelector(".pets").innerHTML = output;
-	}
- }--*/
+ $('#signin, #signup').on(
+	 'click', function(){
+		 $('.signin, .signup').toggle()
+	 }
+ )
 
  //Display pets
 for(let i of pets.data){
@@ -58,9 +29,9 @@ for(let i of pets.data){
 	container.classList.add("container");
 
 	//pet name
-	let name = document.createElement("h5");
+	let name = document.createElement("h4");
 	name.classList.add("pet-name");
-	name.innerText = i.petName.toUpperCase();
+	name.innerText = i.petName;
 	container.appendChild(name);
 
 	//gender
@@ -132,7 +103,7 @@ document.getElementById("search").addEventListener("click", () =>{
             //hide others
             cards[index].classList.add("hide");
         }
-    })
+    }) 
 });
 
 
@@ -141,11 +112,43 @@ window.onload =() => {
     filterProduct("all")
 };
 
- //EventListener for Login Form
- $('.signup').hide();
 
- $('#signin, #signup').on(
-	 'click', function(){
-		 $('.signin, .signup').toggle()
-	 }
- )
+
+
+ /*--//URL
+//alternative two to fetch data from db.json file
+//fetch data
+ let http = new XMLHttpRequest();
+
+ //prepare request with open() method
+ http.open("get", "pets.json", true);
+
+ //send the request
+ http.send();
+
+ //catch the response
+ //check the onload eventListener
+
+ http.onload = function(){
+	//check for ready state and status properties
+	if(this.readyState == 4 && this.status == 200){
+
+		let pets = JSON.parse(this.responseText);
+		let output ="";
+
+		for(let item of pets){
+			output+= `
+			<div class = "pet">
+				<image src="${item.image}" alt="${item.image}">
+				<h4 class="name">${item.name}</h4>
+				<h8 class="gender">${item.gender}</h8>
+				<h5 class="description">${item.description}</h5>
+				<p class ="wishlist">
+				<i class="uil uil-heart"></i></p>
+				<button4>Adopt</button4>
+			</div>
+			`;
+		}
+		document.querySelector(".pets").innerHTML = output;
+	}
+ }--*/
--- pets.js
+++ pets.js
@@ -2,15 +2,77 @@ let pets = {
     data: [
         {
             petName: "Zuri",
-            image: "https://tinyurl.com/2k4m2v6c",
+            image: "https://tinyurl.com/yxtw4vvd",
+            description: "An amazing and playful dog",
+            age:"Young",
+            gender:"Male",
+            category: "Dog",
+        },
+        {
+            petName: "Rehema",
+            image: "https://tinyurl.com/9xax7fp6",
+            description: "Loves snuggles and food",
+            age:"Young",
+            gender:"Female",
+            category: "Cat",
+  
+         },
+        {
+            petName: "Jirani",
+            image: "https://tinyurl.com/2psdbp5s",
+            description: "Loves belly rubs and food",
+            age:"Young",
+            gender:"Male",
+            category: "Other",
+  
+        },
+        {
+            petName: "Kina",
+            image: "https://tinyurl.com/2683n26a",
+            description: "Very Talkative and funny",
+            age:"Young",
+            gender:"Female",
+            category: "Other",
+  
+        },
+        {
+            petName: "Koko",
+            image: "https://tinyurl.com/5hx2wtp9",
             description: "Spot is an amazing and playful dog",
             age:"Young",
             gender:"Male",
             category: "Dog",
         },
         {
-            petName: "Rehema",
-            image: "https://tinyurl.com/9xax7fp6",
+            petName: "Wakachuchu",
+            image: "https://tinyurl.com/55be7aya",
+            description: "Loves snuggles and food",
+            age:"Young",
+            gender:"Female",
+            category: "Cat",
+  
+         },
+        {
+            petName: "Kairetu",
+            image: "https://tinyurl.com/mjmsrwf4",
+            description: "Loves belly rubs and food",
+            age:"Young",
+            gender:"Male",
+            category: "Other",
+  
+        },
+        {
+            petName: "Neema",
+            image: "https://tinyurl.com/2p9sc5pa",
+            description: "Very Talkative and funny",
+            age:"Young",
+            gender:"Female",
+            category: "Dog",
+  
+        },
+        {
+            petName: "Winda",
+            image: "https://tinyurl.com/323nau2f",
             description: "Loves snuggles and food",
             age:"Young",
             gender:"Female",
@@ -19,7 +81,7 @@ let pets = {
          },
         {
             petName: "Azizi",
-            image: "https://tinyurl.com/pmc4b69t",
+            image: "https://tinyurl.com/ycycrnx2",
             description: "Loves belly rubs and food",
             age:"Young",
             gender:"Male",
@@ -27,25 +89,25 @@ let pets = {
   
         },
         {
-            petName: "Kina",
-            image: "https://tinyurl.com/2683n26a",
+            petName: "Amani",
+            image: "https://tinyurl.com/2p9eu6ka",
             description: "Very Talkative and funny",
             age:"Young",
             gender:"Female",
-            category: "Bird",
+            category: "Dog",
   
         },
         {
-            petName: "Zuri",
-            image: "https://tinyurl.com/2k4m2v6c",
+            petName: "Shawty",
+            image: "https://tinyurl.com/ytjywem5",
             description: "Spot is an amazing and playful dog",
             age:"Young",
             gender:"Male",
-            category: "Dog",
+            category: "Other",
         },
         {
-            petName: "Rehema",
-            image: "https://tinyurl.com/9xax7fp6",
+            petName: "Tish",
+            image: "https://tinyurl.com/2s4d4cjy",
             description: "Loves snuggles and food",
             age:"Young",
             gender:"Female",
@@ -53,83 +115,30 @@ let pets = {
   
          },
         {
-            petName: "Azizi",
-            image: "https://tinyurl.com/pmc4b69t",
+            petName: "Simba",
+            image: "https://tinyurl.com/5n7nt549",
             description: "Loves belly rubs and food",
             age:"Young",
             gender:"Male",
+            category: "Other",
+  
+        },
+        {
+            petName: "Hasif",
+            image: "https://tinyurl.com/2539p6ey",
+            description: "Very Talkative and funny",
+            age:"Young",
+            gender:"Female",
             category: "Dog",
   
         },
         {
-            petName: "Kina",
-            image: "https://tinyurl.com/2683n26a",
+            petName: "Pendo",
+            image: "https://tinyurl.com/5cm5dakm",
             description: "Very Talkative and funny",
-            age:"Young",
-            gender:"Female",
-            category: "Bird",
-  
-        },
-        {
-            petName: "Rehema",
-            image: "https://tinyurl.com/9xax7fp6",
-            description: "Loves snuggles and food",
             age:"Young",
             gender:"Female",
             category: "Cat",
-  
-         },
-        {
-            petName: "Azizi",
-            image: "https://tinyurl.com/pmc4b69t",
-            description: "Loves belly rubs and food",
-            age:"Young",
-            gender:"Male",
-            category: "Dog",
-  
-        },
-        {
-            petName: "Kina",
-            image: "https://tinyurl.com/2683n26a",
-            description: "Very Talkative and funny",
-            age:"Young",
-            gender:"Female",
-            category: "Bird",
-  
-        },
-        {
-            petName: "Zuri",
-            image: "https://tinyurl.com/2k4m2v6c",
-            description: "Spot is an amazing and playful dog",
-            age:"Young",
-            gender:"Male",
-            category: "Dog",
-        },
-        {
-            petName: "Rehema",
-            image: "https://tinyurl.com/9xax7fp6",
-            description: "Loves snuggles and food",
-            age:"Young",
-            gender:"Female",
-            category: "Cat",
-  
-         },
-        {
-            petName: "Azizi",
-            image: "https://tinyurl.com/pmc4b69t",
-            description: "Loves belly rubs and food",
-            age:"Young",
-            gender:"Male",
-            category: "Dog",
-  
-        },
-        {
-            petName: "Kina",
-            image: "https://tinyurl.com/2683n26a",
-            description: "Very Talkative and funny",
-            age:"Young",
-            gender:"Female",
-            category: "Bird",
   
         },
     ]   
